7348bc256fddc2a2eeda009e77dd851f304ca12d
Ketan Kauntia <ketankauntia26@gmail.com>
2025-01-29 22:42:43 +0530

    stream added, code later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,5 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div>home page</div>;
 }
